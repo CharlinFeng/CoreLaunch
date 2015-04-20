11d1720daca6c7670c6bd1c2b2d86fa2c97f540a
Charlin_冯成林 <1761904945@qq.com>
2015-04-20 13:56:28 +0800

    Initial Commit

---
+++ CoreAnimLaunchImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimLaunchImageView
+//
+//  Created by 冯成林 on 15/4/20.
+//  Copyright (c) 2015年 冯成林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
